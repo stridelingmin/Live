7af6ca8e38955dff6af8bc1f7da1ee662c83910c
lingmin <lingmin001@hkfs.cn>
2019-03-07 16:01:15 +0800

    Jenkins please retry a build

--- app/src/main/java/cn/stride1025/live/httpUtil.java
+++ app/src/main/java/cn/stride1025/live/httpUtil.java
@@ -5,12 +5,7 @@ public class httpUtil {
     String baseUrl = "http://baidu.com";
 
     String st = "自动化";
-    String sy = "auto";
 
-    int num = 66;
 String sss="dfadsfasdf";
-
-
-String ww = ";";
 
 }
